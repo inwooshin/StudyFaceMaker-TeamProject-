423578eadf5086da7865fbad5cce5e764454be83
hongmin kim <hongmin1998@naver.com>
2020-11-17 23:05:20 +0900

    과제를 해보았습니다.
    과제가 참 어렵네요 출석용 과제인데.

--- buttontest.c
+++ buttontest.c
@@ -8,10 +8,16 @@
 #include <sys/msg.h>
 #include <pthread.h>
 #include "button.h"
+#include "led.h"
+
+#define LED_DRIVER_NAME "/dev/periled"
+#define MAX_LED_NUM 8
 
 
 int main(int argc, char *argv[])
 {
+	ledLibInit();
+	
 	BUTTON_MSG_T B;
 	
 	int msgID = msgget (MESSAGE_ID, IPC_CREAT|0666);
@@ -31,12 +37,18 @@ int main(int argc, char *argv[])
 		if(B.type == EV_KEY){
 			switch(B.keyInput)
 			{
-			case KEY_VOLUMEUP: printf("Volume up key):"); break;
-			case KEY_HOME: printf("Home key):"); break;
-			case KEY_SEARCH: printf("Search key):"); break;
-			case KEY_BACK: printf("Back key):"); break;
-			case KEY_MENU: printf("Menu key):"); break;
-			case KEY_VOLUMEDOWN: printf("Volume down key):"); break;
+			case KEY_HOME: ledOnOff(0x00,1);
+			ledOnOff(0x01,1); 					
+			ledOnOff(0x02,1); 					
+			ledOnOff(0x04,1); 					
+			ledOnOff(0x08,1); 					
+			ledOnOff(0x0F,1); 					
+			break;
+			case KEY_BACK: ledOnOff(0x00,0); break;
+			case KEY_SEARCH: ledOnOff(0x01,1); break;
+			case KEY_MENU: ledOnOff(0x01,0); break;
+			case KEY_VOLUMEUP: ledOnOff(0x02,1); break;
+			case KEY_VOLUMEDOWN: ledOnOff(0x02,0);break;
 			}
 			if ( B.pressed ) printf("pressed\n");
 			else printf("released\n");
