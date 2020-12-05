b593451467058d925fcd9e2acbc7961e4fde618c
hongmin kim <hongmin1998@naver.com>
2020-12-06 01:26:19 +0900

    main함수를 따로 소스코드로 빼고 분리해보았습니다!
    ㅈㄱㄴ

---
+++ embe.h
@@ -0,0 +1,21 @@
+#ifndef _embe_drv_
+#define _embe_drv_
+
+typedef struct{
+	int hour;
+	int min ;
+	int sec ;
+} hourAndMinute;
+
+int goalstudy(void);
+int countup(void);
+int countdown(void);
+int timer(void);
+int daystudy(void);
+int weekstudy(void);
+int goalstudy(void);
+int setInit(void); 
+
+
+
+#endif
--- main.c
+++ main.c
@@ -1,12 +1,80 @@
+#include <getopt.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <stdint.h>
 #include <stdio.h>
-#include "myProject.h"
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h> // for open/close
+#include <fcntl.h> // for O_RDWR
+#include <sys/ioctl.h> // for ioctl
+#include <sys/msg.h>
+#include <pthread.h>
+#include <linux/fb.h>
 
-int main (void){
+
+#include "led.h"
+#include "fnd.h"
+#include "acc.h"
+#include "button.h"
+#include "buzzer.h"
+#include "textlcd.h"
+#include "ColorLed.h"
+#include "Temperature.h"
+#include "bitmap.h"
+#include "embe.h"
+
+hourAndMinute a[10],up;
+int countOut;
+int msgID; // 공부시간을 저장할 변수
+char allStudyToChar[100];
+BUTTON_MSG_T B;
+
+int main(int argc, char* argv[]) {
+   
+	setInit();
 	
-	printf("Chief name is inwoo shin\n");
-	printMyName1();
-	printMyName2();
+	bitmainfunc("MainMenu.bmp");
 	
-	//수정!
+	//tft로 메뉴출력
+	text("main menu", "");
+	buttonStart();
+   
+	int get = 1;
+	while(get > 0){
+		get = msgrcv(msgID, &B, sizeof(unsigned short) * 2 + sizeof(int), 0,IPC_NOWAIT);
+		};
+   
+	while(1){
+		
+		if(countOut){countOut = 0; text("main menu", "");}
 	
+		int returnValue = 0;
+		returnValue = msgrcv(msgID, &B, sizeof(unsigned short) * 2 + sizeof(int), 0, 0);
+		
+		if(B.type == EV_KEY){
+			if ( B.pressed ){
+			bitmainfunc("MainMenu.bmp");
+			switch(B.keyInput)
+			{
+			case KEY_VOLUMEDOWN: 
+			text("program off..", "");
+			 exit(0);
+			break;
+			case KEY_HOME:
+			 timer();	break;
+			case KEY_BACK: daystudy(); break;
+			case KEY_SEARCH:  weekstudy(); break;
+			case KEY_MENU: break;
+			case KEY_VOLUMEUP:  break;
+			}
+		}
+			//if ( B.pressed ) printf("pressed\n");
+			//else printf("released\n");
+		}
+		
+	}
+   
+   return 0;
 }
